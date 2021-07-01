827ef5a2f74c6cc1d81f621c3d3dfe2c068fef13
keiseke <81750996+keiseke@users.noreply.github.com>
2021-07-02 06:10:45 +0900

    HeaderLayout_20210701

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,8 +1,10 @@
 import { memo, VFC } from "react";
 import { Route, Switch } from "react-router-dom";
+
 import { Login } from "../components/pages/Login";
 import { homeRoutes } from "./HomeRoutes";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 
 export const Router: VFC = memo(() => {
   return (
@@ -20,7 +22,7 @@ export const Router: VFC = memo(() => {
                 exact={route.exact}
                 path={`${url}${route.path}`}
               >
-                {route.children}
+                <HeaderLayout>{route.children}</HeaderLayout>
               </Route>
             ))}
           </Switch>
